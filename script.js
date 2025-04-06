027b724ba06fb5f97a91e082834d4c5e8dfa2152
Maria Júlia Barroso <bcmariajulia@gmail.com>
2025-04-06 16:05:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-<script>
-  document.addEventListener("DOMContentLoaded", function () {
-    document.querySelector("#about")?.classList.add("reveal");
-  });
-</script>
-
 function toggleMode() {
     const html = document.documentElement
     /* if(html.classList.contains('light')) {
